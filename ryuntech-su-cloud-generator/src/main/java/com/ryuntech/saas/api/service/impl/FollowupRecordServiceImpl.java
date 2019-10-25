de5e6e9b13ce722c825bf09f0745f9211636f4bb
gaojjbase <290095142@qq.com>
2019-10-25 11:12:28 +0800

    小程序合同功能修改

---
+++ ryuntech-su-cloud-generator/src/main/java/com/ryuntech/saas/api/service/impl/FollowupRecordServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ryuntech.saas.api.service.impl;
+
+import com.ryuntech.saas.api.entity.FollowupRecord;
+import com.ryuntech.saas.api.mapper.FollowupRecordMapper;
+import com.ryuntech.saas.api.service.IFollowupRecordService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 跟进表 服务实现类
+ * </p>
+ *
+ * @author antu
+ * @since 2019-10-24
+ */
+@Service
+public class FollowupRecordServiceImpl extends ServiceImpl<FollowupRecordMapper, FollowupRecord> implements IFollowupRecordService {
+
+}
