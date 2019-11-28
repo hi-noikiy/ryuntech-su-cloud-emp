cd7cfd7f171e4cb98a9bb31b88c428bd6ea65e65
wanh <851597995@qq.com>
2019-11-28 18:02:27 +0800

    请求异常处理

--- ryuntech-su-cloud-service-saas-web/src/api/constant.js
+++ ryuntech-su-cloud-service-saas-web/src/api/constant.js
@@ -1,12 +1,12 @@
 
 // Spring Boot Admin
-export const admin = 'http://wx.ryuntech.com:3000/'
+export const admin = 'https://wx.ryuntech.com:3000/'
 
 // ZipKin
-export const zipkin = 'http://wx.ryuntech.com:3001/'
+export const zipkin = 'https://wx.ryuntech.com:3001/'
 
 // Eureka
-export const eureka = 'http://wx.ryuntech.com:8761/'
+export const eureka = 'https://wx.ryuntech.com:8761/'
 
 export const auth = 'https://wx.ryuntech.com:9999/api/auth/'
 
