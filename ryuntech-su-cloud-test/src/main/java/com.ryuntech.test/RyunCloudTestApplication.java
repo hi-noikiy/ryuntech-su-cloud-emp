439c554e610301ead8f24ee485ed55d6ee097af5
wanh <851597995@qq.com>
2019-12-11 19:18:03 +0800

    Merge branch 'master' of C:\gitproject\backend\ryuntech-su-cloud-emp with conflicts.

--- ryuntech-su-cloud-test/src/main/java/com.ryuntech.test/RyunCloudTestApplication.java
+++ ryuntech-su-cloud-test/src/main/java/com.ryuntech.test/RyunCloudTestApplication.java
@@ -1,24 +1,23 @@
-package com.ryuntech.zipkin;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-/**
- * 链路追踪 ZipKin
- *
- * @author antu
- * @date 2019-05-21
- */
-//@EnableRyunResourceServer
-//@EnableFeignClients
-//@EnableEurekaClient
-@SpringBootApplication
-//@EnableTransactionManagement
-@ComponentScan(basePackages = {"com.ryuntech.saas.api","com.ryuntech.saas"})
-public class RyunCloudTestApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(RyunCloudTestApplication.class, args);
-    }
-}
+//package com.ryuntech.test;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * 链路追踪 ZipKin
+// *
+// * @author antu
+// * @date 2019-05-21
+// */
+////@EnableRyunResourceServer
+////@EnableFeignClients
+////@EnableEurekaClient
+//@SpringBootApplication
+////@EnableTransactionManagement
+////@ComponentScan(basePackages = {"com.ryuntech.saas.api","com.ryuntech.saas"})
+//public class RyunCloudTestApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(RyunCloudTestApplication.class, args);
+//    }
+//}
